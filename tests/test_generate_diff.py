231ea2a4961145b25678be3ee23b23421d4bec41
vova <vladimir1012kov@gmail.com>
2022-04-21 17:17:47 +0300

    the library for working with the yml file has been added, tests have been rewritten, yaml fixtures have been prepared

--- tests/test_generate_diff.py
+++ tests/test_generate_diff.py
@@ -1,14 +1,23 @@
 from gendiff.app_functionality import gendiff_logic
 
 
-def test_gendiff():
-    file1 = 'tests/fixtures/file1.json'
-    file2 = 'tests/fixtures/file2.json'
-    file3 = open_file('tests/fixtures/file_test_gendiff.txt')
-    assert gendiff_logic.generate_diff(file1, file2) == file3
-
-
 def open_file(name_file):
     file_on = open(name_file, 'r')
     return ''.join([i for i in file_on])
+
+
+file3 = open_file('tests/fixtures/file_test_gendiff.txt')
+
+
+def test_gendiff_json():
+    file1 = 'tests/fixtures/file1.json'
+    file2 = 'tests/fixtures/file2.json'
+    assert gendiff_logic.generate_diff(file1, file2) == file3
+
+
+def test_gendiff_yaml():
+    file1 = 'tests/fixtures/file1.yml'
+    file2 = 'tests/fixtures/file2.yml'
+    assert gendiff_logic.generate_diff(file1, file2) == file3
+
     
